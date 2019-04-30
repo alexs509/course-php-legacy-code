127752c80f5ecb442425356003985952907024a8
Alex <alex.rigueur@gmail.com>
2019-04-30 11:03:18 +0200

    add service container dependance

---
+++ www/config/global.php
@@ -0,0 +1,11 @@
+<?php
+
+return  [
+    'database' =>   [
+        'driver' => 'mysql',
+        'host' => 'database',
+        'name' => 'course-php-legacy-code',
+        'user' => 'root',
+        'password' => 'course-legacy',
+    ]
+];
