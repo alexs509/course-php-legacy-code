5deb939666bd8b99ef030a027dee7d766fbcccb3
alexs509 <alex.rigueur@gmail.com>
2019-05-09 07:50:22 +0200

    fix bug after opening modal

--- www/core/BaseSQL.class.php
+++ www/core/BaseSQL.class.php
@@ -10,8 +10,8 @@ class BaseSQL
     public function __construct()
     {
         try {
-            $this->pdo = new PDO(DBDRIVER.':host='.DBHOST.';dbname='.DBNAME, DBUSER, DBPWD);
-            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->pdo = new \PDO(DBDRIVER.':host='.DBHOST.';dbname='.DBNAME, DBUSER, DBPWD);
+            $this->pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
         } catch (Exception $e) {
             die('Erreur SQL : '.$e->getMessage());
         }
