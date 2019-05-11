104fa4ce4a2cd65f9aa015a229ccbc07af2db319
alexs509 <alex.rigueur@gmail.com>
2019-05-11 23:42:33 +0200

    create new class

--- www/core/BaseSQL.class.php
+++ www/core/BaseSQL.class.php
@@ -30,49 +30,4 @@ class BaseSQL
         $this->getOneBy(['id' => $id], true);
     }
 
-    /**
-     * @param array $where  the where clause
-     * @param bool  $object if it will return an array of results ou an object
-     *
-     * @return mixed
-     */
-    public function getOneBy(array $where, $object = false): array
-    {
-        $sqlWhere = [];
-        foreach ($where as $key => $value) {
-            $sqlWhere[] = $key.'=:'.$key;
-        }
-        $sql = ' SELECT * FROM '.$this->table.' WHERE  '.implode(' AND ', $sqlWhere).';';
-        $query = $this->pdo->prepare($sql);
-
-        if ($object) {
-            $query->setFetchMode(PDO::FETCH_INTO, $this);
-        } else {
-            $query->setFetchMode(PDO::FETCH_ASSOC);
-        }
-        $query->execute($where);
-        return $query->fetch();
-    }
-
-    public function save(): void
-    {
-        $dataObject = get_object_vars($this);
-        $dataChild = array_diff_key($dataObject, get_class_vars(get_class()));
-
-        if (is_null($dataChild['id'])) {
-            $sql = 'INSERT INTO '.$this->table.' ( '.
-            implode(',', array_keys($dataChild)).') VALUES ( :'.
-            implode(',:', array_keys($dataChild)).')';
-        } else {
-            $sqlUpdate = [];
-            foreach ($dataChild as $key => $value) {
-                if ('id' != $key) {
-                    $sqlUpdate[] = $key.'=:'.$key;
-                }
-            }
-            $sql = 'UPDATE '.$this->table.' SET '.implode(',', $sqlUpdate).' WHERE id=:id';
-        }
-        $query = $this->pdo->prepare($sql);
-        $query->execute($dataChild);
-    }
 }
